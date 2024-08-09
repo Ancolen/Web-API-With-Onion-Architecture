3c153df0561fba45563d0b13a795c8dbdbe1c404
Ancolen <mertilker96@gmail.com>
2024-08-09 23:14:38 +0300

    common, entities

---
+++ Core/Domain/Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category() { }
+        public Category(int parentId, string name, int queueId)
+        {
+            ParentID = parentId;
+            Name = name;
+            QueueID = queueId;
+        }
+        public required int ParentID { get; set; }
+        public required string Name { get; set; }
+        public required int QueueID { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
